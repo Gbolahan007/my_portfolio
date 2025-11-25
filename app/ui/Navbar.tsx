5a8dafb3f79be7a46c4a482aec5946444de9bc4f
Lawal Omogbolahan <lawalomogbolahan08@gmail.com>
2025-11-25 14:37:52 +0100

    updated scroll finish

--- app/ui/Navbar.tsx
+++ app/ui/Navbar.tsx
@@ -1,9 +1,10 @@
 "use client";
 
 import type React from "react";
-import { useRef } from "react";
+import { useRef, useEffect } from "react";
 import gsap from "gsap";
 import { useGSAP } from "@gsap/react";
+import { useScrollDirection } from "./hooks/useScrollDirection";
 
 interface NavbarProps {
   onMenuOpen: () => void;
@@ -12,8 +13,8 @@ interface NavbarProps {
 
 export default function Navbar({ onMenuOpen, onNavClick }: NavbarProps) {
   const navRef = useRef(null);
+  const { isVisible } = useScrollDirection();
 
-  // NAVBAR FADE-IN ANIMATION
   useGSAP(() => {
     gsap.from(navRef.current, {
       y: -70,
@@ -22,6 +23,17 @@ export default function Navbar({ onMenuOpen, onNavClick }: NavbarProps) {
       ease: "power3.out",
     });
   }, []);
+
+  // SHOW/HIDE NAVBAR ON SCROLL
+  useEffect(() => {
+    if (!navRef.current) return;
+
+    gsap.to(navRef.current, {
+      y: isVisible ? 0 : -100, // hide it upwards
+      duration: 0.4,
+      ease: "power2.out",
+    });
+  }, [isVisible]);
 
   const handleNavHover = (e: React.MouseEvent<HTMLAnchorElement>) => {
     const target = e.currentTarget;
@@ -81,10 +93,9 @@ export default function Navbar({ onMenuOpen, onNavClick }: NavbarProps) {
   return (
     <nav
       ref={navRef}
-      className="sm:max-w-7xl mt-5 flex justify-center fixed top-0 left-0 right-0 z-50"
+      className=" max-w-7xl mt-5 flex justify-center fixed top-0 left-0 right-0 z-50 mx-4"
     >
-      <div className="flex items-center justify-between w-full mx-3 md:justify-center md:w-auto gap-8 bg-zinc-900 rounded-sm px-8 py-4 border border-zinc-800">
-        {/* LOGO (MOBILE ONLY) */}
+      <div className="flex items-center justify-between w-full md:justify-center md:w-auto gap-8 bg-zinc-900 rounded-sm px-8 py-4 border border-zinc-800">
         <div className="md:hidden text-zinc-400 text-xl font-mono">
           &lt;/&gt;
         </div>

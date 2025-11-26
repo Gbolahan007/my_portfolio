374969b9c22517bf1b7ccb8b79f220128c73bd36
Lawal Omogbolahan <lawalomogbolahan08@gmail.com>
2025-11-26 16:37:35 +0100

    updated fav

--- app/layout.tsx
+++ app/layout.tsx
@@ -94,13 +94,15 @@ export const metadata: Metadata = {
   icons: {
     icon: [
       { url: "/favicon.ico", sizes: "any" },
-      { url: "/icon.png", type: "image/png", sizes: "32x32" },
-      { url: "/icon.svg", type: "image/svg+xml" },
+
+      { url: "/favicon-16x16.png", type: "image/png", sizes: "16x16" },
+      { url: "/favicon-32x32.png", type: "image/png", sizes: "32x32" },
     ],
     shortcut: "/favicon.ico",
     apple: [
       { url: "/apple-touch-icon.png", sizes: "180x180", type: "image/png" },
     ],
+
     other: [
       {
         rel: "mask-icon",
